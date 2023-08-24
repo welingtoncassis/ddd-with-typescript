7267aad35a77a0c579f833299e90bfd48959ad2d
welingtoncassis <welingtoncassis@gmail.com>
2023-08-24 20:29:12 -0300

    feat: remove entidade anêmica e add regras de negócio

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,34 +2,24 @@ class Customer {
   _id: string;
   _name: string;
   _address: string;
+  _active: boolean;
 
   constructor(id: string, name: string, address: string) {
     this._id = id;
     this._name = name;
     this._address = address;
+    this._active = true;
   }
 
-  get id(): string {
-    return this._id;
+  changeName(newName: string) {
+    this._name = newName;
   }
 
-  get name(): string {
-    return this._name;
+  activate() {
+    this._active = true;
   }
 
-  get address(): string {
-    return this._address;
-  }
-
-  set id(id: string) {
-    this._id = id;
-  }
-
-  set name(name: string) {
-    this._name = name;
-  }
-
-  set address(address: string) {
-    this._address = address;
+  deactivate() {
+    this._active = false;
   }
 }
