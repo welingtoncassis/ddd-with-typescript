8bc84801bc4a676e1ae3ef7e55899421f94b6145
welingtoncassis <welingtoncassis@gmail.com>
2023-10-19 17:28:40 -0300

    feat(order-service): add rewardPoints

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,19 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/orderItem";
 import OrderService from "./order.service";
 
 describe("OrderService unit test", () => {
+  it("should place an order", () => {
+    const customer = new Customer("c1", "Customer 1");
+    const item1 = new OrderItem("i1", "p1", "Item 1", 10, 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  });
+
   it("should get total of all orders", () => {
     const item1 = new OrderItem("i1", "p1", "Item 1", 100, 1);
     const item2 = new OrderItem("i2", "p2", "Item 2", 200, 2);
--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,9 +1,22 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
+import OrderItem from "../entity/orderItem";
+import { v4 as uuidv4 } from "uuid";
 
 export default class OrderService {
-  static total(orders: Order[]) {
+  static total(orders: Order[]): number {
     return orders.reduce((total, order) => {
       return total + order.total();
     }, 0);
+  }
+
+  static placeOrder(customer: Customer, items: OrderItem[]): Order {
+    if (items.length === 0) {
+      throw new Error("Order must have at least one item");
+    }
+
+    const order = new Order(uuidv4(), customer.id, items);
+    customer.addRewardPoints(order.total() / 2);
+    return order;
   }
 }
