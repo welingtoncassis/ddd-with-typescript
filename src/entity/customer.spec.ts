9d31fd7b73158f035be9ef9301216965fa45a041
welingtoncassis <welingtoncassis@gmail.com>
2023-10-19 17:31:38 -0300

    test(customer): add addRewardPoints() test

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -43,4 +43,15 @@ describe("Customer unit tests", () => {
       customer.activate();
     }).toThrowError("Address is mandatory to active");
   });
+
+  it("should add reward points", () => {
+    const customer = new Customer("123", "John");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
+  });
 });
